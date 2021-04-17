c9b25b8a7c03b7be9158fbdedddd5c82b87986bd
pingbtw <exiled.developer@gmail.com>
2021-04-17 10:09:14 -0400

    Access other user points

--- src/main/java/me/ping/bot/listeners/Points.java
+++ src/main/java/me/ping/bot/listeners/Points.java
@@ -2,19 +2,29 @@ package me.ping.bot.listeners;
 
 import me.ping.bot.core.DbHandler;
 import me.ping.bot.core.QueryResult;
+import me.ping.bot.core.StringUtils;
 import me.ping.bot.exceptions.InvalidDataTypeException;
 import me.ping.bot.exceptions.ParameterCountMismatchException;
+import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.TextChannel;
+import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 import java.sql.SQLException;
+import java.util.List;
 
 public class Points extends ListenerAdapter {
     public void onMessageReceived(MessageReceivedEvent event) {
         insertIntoPointsTable(event);
-        if (event.getMessage().getContentRaw().equalsIgnoreCase("-points")) {
-            getUserPoints(event);
+        if (StringUtils.startsWithIgnoreCase(event.getMessage().getContentRaw(), "-points")) {
+            if (event.getMessage().getMentionedMembers().size() > 1) {
+                event.getChannel().sendMessage("Too many users provided").queue();
+            } else if (event.getMessage().getMentionedMembers().isEmpty()) {
+                getUserPoints(event);
+            } else {
+                getUserPoints(event, event.getMessage().getMentionedMembers().get(0));
+            }
         }
     }
 
@@ -37,7 +47,29 @@ public class Points extends ListenerAdapter {
             // do nothing jon snow
             e.printStackTrace();
         }
-        event.getChannel().sendMessage("You have " + points + " points").queue();
+        event.getChannel().sendMessage("You have " + points + " point(s)").queue();
+    }
+
+    public void getUserPoints(MessageReceivedEvent event, Member member) {
+        long points = 0;
+        try {
+            DbHandler db = DbHandler.getInstance();
+            String sql = "SELECT * FROM points WHERE user_id=?";
+            QueryResult qr = db.select(sql, member.getIdLong());
+            if (qr.hasResultSet()) {
+                while (qr.getRs().next()) {
+                    points = qr.getRs().getInt("points");
+                }
+            }
+            qr.close();
+            db.close();
+        } catch (InvalidDataTypeException | ParameterCountMismatchException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            // do nothing jon snow
+            e.printStackTrace();
+        }
+        event.getChannel().sendMessage(member.getAsMention() + " has " + points + " point(s)").queue();
     }
 
     public void insertIntoPointsTable(MessageReceivedEvent event) {
